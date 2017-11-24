86b7b24d423359816f41048447d878b2e6623840
ouril <ourilkregg@gmail.com>
2017-11-25 00:58:00 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename:'bundle.js',
+  },
+  module: {
+  rules: [
+    { test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader", "react-hot"] }
+  ]
+}
+}
